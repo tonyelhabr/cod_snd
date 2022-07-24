b4047e3370af9071fc2e724fc83e0223703b288e
Tony ElHabr <anthonyelhabr@gmail.com>
2022-07-23 20:18:06 -0500

    val cleanup

--- 1-scrape-valorant.R
+++ 1-scrape-valorant.R
@@ -91,6 +91,10 @@ init_val_rounds_side <- raw_val_rounds |>
     )
   )
 
+## note that the notion of a series may be confusing. in valorant, we typically think
+##  of a match/game as the first to 13 round wins, and the series as the first to 2 match/game/map wins.
+##  however, i'm treating match/game/map as a "series" since we're not really interested in cumulative
+##  match/game/map wins between 2 teams.
 val_series_outcomes <- init_val_rounds_side |>
   group_by(game_id) |>
   slice_max(round, n = 1, with_ties = FALSE) |>
@@ -104,54 +108,20 @@ val_series_outcomes <- init_val_rounds_side |>
 weird_val_series_outcomes <- val_series_outcomes |>
   filter(max_cumu_wl > 13 & (min_cumu_wl < (max_cumu_wl - 2)))
 
-val_team_abbrvs <- val_scoreboard |> 
-  distinct(game_id, team_abbrv = team_abbreviation) |> 
-  mutate(across(team_abbrv, ~toupper(.x) |> str_squish()))
-
-raw_val_team_mapping <- bind_rows(
-  val_games |>
-    distinct(game_id, team_id = team1id, team = team1),
-  val_games |>
-    distinct(game_id, team_id = team2id, team = team2)
-) |>
-  distinct(game_id, team_id, team) |>
-  arrange(team_id) |> 
-  inner_join(val_team_abbrvs, by = 'game_id') |> 
-  distinct(game_id, team_id, team, team_abbrv) |> 
-  count(team_id, team, team_abbrv) |> 
-  group_by(team_id, team) |> 
-  slice_max(n, n = 1, with_ties = FALSE) |> 
-  ungroup() |> 
-  arrange(desc(n))
-
-# val_team_mapping <- raw_val_team_mapping |> 
-#   filter(team_abbrv != '', n >= 5)
-val_team_mapping <- raw_val_team_mapping
-
-val_games |> 
-  inner_join(
-    val_team_mapping |> distinct(winner = team, winner_abbrv = team_abbrv),
-    by = 'winner'
-  ) |> 
-  count(game_id, match_id, winner, winner_abbrv, sort = TRUE)
-
-val_games |> 
-  transmute(
-    game_id,
-    team1,
-    team2,
-    team1_side_first_half,
-    total_rounds = team1_total_rounds + team2_total_rounds
-  ) |> 
-  inner_join(
-    val_series_outcomes |> 
-      select(game_id, last_round = round, series_winner) |> 
-      inner_join(
-        val_team_mapping |> select(team, series_winner = team_abbrv), 
-        by = 'series_winner'
-      ),
-    by = 'game_id'
-  )
+val_team_mapping <- bind_rows(
+  init_val_rounds_side |> 
+    group_by(game_id) |> 
+    filter(lag(cumu_w) != cumu_w) |> 
+    ungroup() |> 
+    distinct(game_id, team_id = team1id, team = round_winner),
+  init_val_rounds_side |> 
+    group_by(game_id) |>
+    filter(lag(cumu_l) != cumu_l) |> 
+    ungroup() |> 
+    distinct(game_id, team_id = team2id, team = round_winner)
+) |> 
+  distinct(game_id, team_id, team) |> 
+  arrange(game_id, team_id)
 
 ## note that i've decided to use val_rounds -> val_series_outcomes for determining the last round
 ##   instead of val_matches. this is more self coherent and i think val_matches has some errors.
@@ -160,15 +130,14 @@ val_games |>
 ##   liquepedia (https://liquipedia.net/valorant/VALORANT_Champions_Tour/2021/Southeast_Asia/Stage_2/Challengers_3/Malaysia_and_Singapore) has this as ending in 17 rounds, as well as val_matches.
 ##   but vlr.gg (https://www.vlr.gg/14384/paper-rex-vs-team-smg-champions-tour-malaysia-singapore-stage-2-challengers-3-main-event-grand/?game=25724&tab=overview) has this as ending in 19 rounds, as well as val_rounds.
 # init_val_rounds_side |> filter(game_id == 25726)
-
 val_rounds_side <- init_val_rounds_side |> 
   inner_join(
-    val_team_mapping |> select(team1id = team_id, team_1 = team_abbrv), 
-    by = 'team1id'
+    val_team_mapping |> select(game_id, team1id = team_id, team_1 = team), 
+    by = c('game_id', 'team1id')
   ) |> 
   inner_join(
-    val_team_mapping |> select(team2id = team_id, team_2 = team_abbrv), 
-    by = 'team2id'
+    val_team_mapping |> select(game_id, team2id = team_id, team_2 = team), 
+    by = c('game_id', 'team2id')
   ) |> 
   inner_join(
     val_matches |> 
@@ -201,7 +170,7 @@ val_rounds_side <- init_val_rounds_side |>
     by = c('game_id', 'match_id')
   ) |> 
   transmute(
-    game_id,
+    series_id = game_id,
     match_id,
     team_1,
     team_2,
@@ -219,30 +188,36 @@ val_rounds_side <- init_val_rounds_side |>
     win_round_1 = team_1 == round_winner,
     win_series_1 = team_1 == series_winner,
     team_1_series_w,
-    team_2_series_w
+    team_2_series_w,
+    team_1_buy_type = team1buy_type,
+    team_2_buy_type = team2buy_type,
+    team_1_bank = team1bank,
+    team_2_bank = team2bank,
+    win_type
   ) |> 
-  arrange(game_id, round) |> 
-  group_by(game_id) |> 
+  arrange(series_id, round) |> 
+  group_by(series_id) |> 
   mutate(
     pre_cumu_w = lag(cumu_w, default = 0L),
     pre_cumu_l = lag(cumu_l, default = 0L)
   ) |> 
   ungroup()
-val_rounds_side |> 
-  filter(!is_offense_1) |> 
-  filter(cumu_l == 1, cumu_w == 0) |> 
-  count(win_round_1)
+val_rounds_side
 
 val_rounds <- bind_rows(
   val_rounds_side |> 
     select(
-      game_id,
-      match_id,
+      series_id,
       team = team_1,
       opponent = team_2,
       map,
       round,
       is_offense = is_offense_1,
+      team_buy_type = team_1_buy_type,
+      opponent_buy_type = team_2_buy_type,
+      team_bank = team_1_bank,
+      opponent_bank = team_2_bank,
+      win_type,
       win_round = win_round_1,
       win_series = win_series_1,
       pre_cumu_w,
@@ -254,13 +229,17 @@ val_rounds <- bind_rows(
     ),
   val_rounds_side |> 
     transmute(
-      game_id,
-      match_id,
+      series_id,
       team = team_2,
       opponent = team_1,
       map,
       round,
       is_offense = !is_offense_1,
+      team_buy_type = team_2_buy_type,
+      opponent_buy_type = team_1_buy_type,
+      team_bank = team_2_bank,
+      opponent_bank = team_1_bank,
+      win_type,
       win_round = !win_round_1,
       win_series = !win_series_1,
       pre_cumu_w,
@@ -286,6 +265,14 @@ val_rounds <- bind_rows(
     select(-c(pre_cumu_z, cumu_z))
 ) |> 
   inner_join(
+    val_rounds_side |> 
+      group_by(series_id, match_id) |> 
+      slice_max(round, n = 1, with_ties = FALSE) |> 
+      ungroup() |> 
+      select(series_id, match_id, max_round = round),
+    by = 'series_id'
+  ) |> 
+  inner_join(
     val_matches |> 
       transmute(
         match_id,
@@ -298,10 +285,9 @@ val_rounds <- bind_rows(
       ),
     by = 'match_id'
   ) |> 
-  arrange(date, game_id, round, is_offense) |> 
-  select(
-    game_id,
-    match_id,
+  arrange(date, series_id, round, is_offense) |> 
+  transmute(
+    series_id,
     event_id,
     event_name,
     event_stage,
@@ -310,7 +296,12 @@ val_rounds <- bind_rows(
     team,
     opponent,
     round,
+    max_round,
     is_offense,
+    team_buy_type,
+    opponent_buy_type,
+    team_bank,
+    opponent_bank,
     win_round,
     win_series,
     pre_cumu_w,
@@ -319,5 +310,22 @@ val_rounds <- bind_rows(
     cumu_l,
     team_series_w,
     opponent_series_w
+  ) |> 
+  anti_join(
+    weird_val_series_outcomes,
+    by = c('round', 'cumu_w', 'cumu_l')
   )
 qs::qsave(val_rounds, 'valorant_rounds.qs')
+
+# val_rounds |> 
+#   filter(is_offense) |> 
+#   mutate(
+#     across(
+#       c(team_buy_type, opponent_buy_type),
+#       ~case_when(
+#         .x %in% c('full-buy', 'eco') ~ .x,
+#         TRUE ~ 'other'
+#       )
+#     )
+#   ) |> 
+#   count(team_buy_type, opponent_buy_type, sort = TRUE)
