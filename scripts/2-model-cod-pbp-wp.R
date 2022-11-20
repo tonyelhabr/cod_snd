9d64d2b26bf590691d49f0de8f966b3eff6c654f
Tony ElHabr <anthonyelhabr@gmail.com>
2022-11-20 11:16:00 -0600

    major refactor, combining interaces to xgb and lb methods

--- scripts/2-model-cod-pbp-wp.R
+++ scripts/2-model-cod-pbp-wp.R
@@ -18,6 +18,7 @@ source('scripts/helpers-wp.R')
 
 both_pbp <- read_csv('data/cod_snd_pbp.csv', show_col_types = FALSE)
 
+## naive round win rates ----
 both_pbp |> 
   # filter(side == 'd') |> 
   # distinct(round_id)
@@ -28,16 +29,6 @@ both_pbp |>
     prop = n / sum(n)
   ) |> 
   ungroup()
-
-both_pbp |>
-  arrange(round_id, pre_plant_seconds_elapsed, post_plant_seconds_elapsed) |> 
-  group_by(round_id, side) |> 
-  mutate(
-    across(c(n_team_remaining, n_opponent_remaining), list(prev = ~lag(.x, n = 1, default = 4L)))
-  ) |> 
-  ungroup() |> 
-  filter(n_team_remaining_prev == 0L | n_opponent_remaining_prev == 0L, activity != 'Defuse') |> 
-  count(activity, weapon_or_bomb_site)
 
 round_win_prop_by_xvy <- both_pbp |>
   arrange(round_id, pre_plant_seconds_elapsed, post_plant_seconds_elapsed) |> 
@@ -61,10 +52,7 @@ wide_round_win_prop_by_xvy <- round_win_prop_by_xvy |>
     values_fill = list(n = 0L, prop = 0)
   )
 
-both_pbp |> 
-  filter(pbp_side == 'a', n_team_remaining == 0, n_opponent_remaining == 0) |> 
-  glimpse()
-
+## model data prep ----
 init_model_pbp <- both_pbp |> 
   mutate(
     across(
@@ -129,282 +117,25 @@ stopifnot(0 == (all_model_pbp |> filter(!is_pre_plant, !is_post_plant) |> distin
 ## should only be plant activities
 stopifnot(1 == (all_model_pbp |> filter(is_pre_plant, is_post_plant) |> count(activity) |> nrow()))
 
-model_pbp <- all_model_pbp |> 
-  filter(side == 'o') |> 
-  filter(activity == 'Kill')
+## model ----
+model_lb <- all_model_pbp |> fit_wp_model_lb()
+# augment(model_lb, all_model_pbp)
+wp_grid_lb <- generate_wp_grid(model_lb)
+autoplot(wp_grid_lb)
 
-last_pre_plant_seconds_elapsed <- model_pbp |> 
-  filter(is_pre_plant) |> 
-  group_by(round_id) |> 
-  slice_max(seconds_elapsed, n = 1) |> 
-  ungroup() |> 
-  select(round_id, round_has_plant, last_pre_plant_seconds_elapsed = seconds_elapsed)
-
-last_post_plant_seconds_elapsed <- model_pbp |> 
-  filter(round_has_plant, !is_pre_plant) |> 
-  group_by(round_id) |> 
-  slice_max(seconds_elapsed, n = 1) |> 
-  ungroup() |> 
-  select(round_id, last_post_plant_seconds_elapsed = seconds_elapsed)
-
-pre_plant_model_pbp <- model_pbp |> 
-  inner_join(
-    last_pre_plant_seconds_elapsed,
-    by = 'round_id'
-  ) |> 
-  filter(is_pre_plant) |> 
-  select(-c(is_post_plant)) 
-
-# pre_plant_seconds_coefs <- pre_plant_model_pbp |> 
-#   estimate_window_coefs(is_pre_plant = TRUE, overwrite = TRUE)
-
-## ----
-library(tidymodels)
-
-round_ids <- pre_plant_model_pbp |> distinct(round_id) |> pull(round_id)
-set.seed(42)
-round_ids_trn <- sample(round_ids, size = 0.75 * length(round_ids))
-trn <- pre_plant_model_pbp |> filter(round_id %in% round_ids_trn)
-tst <- pre_plant_model_pbp |> filter(!(round_id %in% round_ids_trn))
-
-rec <- recipe(
-  win_round ~ 
-    seconds_elapsed +
-    prev_opponent_diff +
-    is_initial_bomb_carrier_killed +
-    is_during_attempted_plant +
-    is_during_attempted_defuse, 
-  data = trn
-)
-
-spec <- boost_tree(mode = 'classification') |> 
-  set_engine(engine = 'xgboost', monotone_constraints = c(1, -1, -1, 1, -1)) # 1, -1))
-wf <- workflow(preprocessor = rec, spec = spec)
-fit <- fit(wf, trn)
-pre_plant_grid_preds <- generate_pred_grid(fit, is_pre_plant = TRUE)
-pre_plant_grid_preds$wp <- predict(fit, pre_plant_grid_preds, type = 'prob')$`.pred_yes`
-
-pre_plant_grid_preds <- generate_pred_grid(fit, is_pre_plant = TRUE)
-pre_plant_grid_preds$wp <- predict(fit, pre_plant_grid_preds, type = 'prob')$`.pred_yes`
-
-summarize_pred_grid_across_features <- function(df, col) {
-  df |> 
-    group_by(seconds_elapsed, prev_opponent_diff, value = .data[[col]]) |> 
-    summarize(
-      across(wp, mean)
-    ) |> 
-    ungroup()
-}
-
-c(
-  'is_initial_bomb_carrier_killed',
-  'is_during_attempted_defuse', 
-  'is_during_attempted_plant'
-) |> 
-  set_names() |> 
-  map_dfr(
-    ~summarize_pred_grid_across_features(pre_plant_grid_preds, .x), 
-    .id = 'feature'
-  ) |> 
-  unite('feature_value', feature, value) |> 
-  ggplot() +
-  aes(x = seconds_elapsed, y = wp) +
-  geom_hline(
-    color = 'white',
-    aes(yintercept = 0.5)
-  ) +
-  geom_step(
-    size = 1,
-    aes(color = factor(prev_opponent_diff))
-  ) +
-  guides(
-    color = guide_legend(
-      title = 'Net # of players',
-      override.aes = list(size = 3)
-    )
-  ) +
-  ggsci::scale_color_tron() +
-  scale_y_continuous(labels = scales::comma) +
-  facet_wrap(~feature_value, dir = 'v', nrow = 2) +
-  coord_cartesian(ylim = c(0, 1)) +
-  theme(
-    legend.position = 'top'
-  ) +
-  labs(
-    title = 'Offensive Win Probability',
-    x = 'Seconds Elapsed',
-    y = 'Win Probability'
-  )
-
-pre_plant_grid_preds |> 
-  filter(
-    is_during_attempted_defuse == 0, 
-    is_during_attempted_plant == 0, 
-    is_initial_bomb_carrier_killed == 0
-  ) |> 
-  # count(seconds_elapsed, prev_opponent_diff)
-  ggplot() +
-  aes(x = seconds_elapsed, y = wp) +
-  geom_hline(
-    color = 'white',
-    aes(yintercept = 0.5)
-  ) +
-  geom_step(
-    size = 1.5,
-    aes(group = factor(prev_opponent_diff), color = factor(prev_opponent_diff))
-  ) +
-  guides(
-    color = guide_legend(
-      title = 'Net # of players', 
-      override.aes = list(size = 3)
-    )
-  ) +
-  ggsci::scale_color_tron() +
-  scale_y_continuous(labels = scales::comma) +
-  coord_cartesian(ylim = c(0, 1)) +
-  theme(
-    legend.position = 'top'
-  ) +
-  labs(
-    title = 'Offensive Win Probability',
-    x = 'Seconds Elapsed',
-    y = 'Win Probability'
-  )
-
-## ----
-pre_plant_model <- fit_wp_model(pre_plant_seconds_coefs)
-pre_plant_grid_preds <- generate_wp_grid(pre_plant_model, is_pre_plant = TRUE)
-pre_plant_grid_preds |> tail(20)
-pre_plant_grid_preds |> 
-  filter(is_offense == 1L, is_initial_bomb_carrier_killed == 0L, is_during_attempted_plant == 0L) |> 
-  plot_wp_grid() +
-  labs(title = 'Offense Pre-Plant Win Probability')
 ggsave(
-  filename = file.path('figs', 'pre_plant_offensive_wp.png'),
+  filename = file.path('figs', 'wp_grid_lb.png'),
   width = 8,
   height = 6
 )
 
-post_plant_pbp <- model_pbp |> 
-  inner_join(
-    seconds_elapsed |> 
-      filter(is_plant_in_round) |> 
-      distinct(round_id, last_seconds_elapsed = last_post_plant_seconds_elapsed),
-    by = 'round_id'
-  ) |> 
-  rename( 
-    round_seconds_elapsed = seconds_elapsed,
-    seconds_elapsed = post_plant_seconds_elapsed
-  ) |> 
-  filter(is_post_plant) |> 
-  select(-c(pre_plant_seconds_elapsed, is_post_plant)) 
+model_xgb <- all_model_pbp |> fit_wp_model_xgb(tune = FALSE)
+# augment(model_xgb, all_model_pbp) |> slice_max(wp) |> glimpse()
+wp_grid_xgb <- generate_wp_grid(model_xgb)
+autoplot(wp_grid_xgb)
 
-post_plant_seconds_coefs <- post_plant_pbp |> 
-  filter(seconds_elapsed != last_seconds_elapsed) |> ## drop the last kill
-  select(-last_seconds_elapsed) |> 
-  estimate_window_coefs(is_pre_plant = FALSE, overwrite = TRUE)
-
-post_plant_model <- fit_wp_model(post_plant_seconds_coefs)
-post_plant_grid_preds <- generate_wp_grid(post_plant_model, is_pre_plant = FALSE)
-plot_wp_grid(post_plant_grid_preds) +
-  labs(title = 'Offense Post-Plant Win Probability')
 ggsave(
-  filename = file.path('figs', 'post_plant_offensive_wp.png'),
+  filename = file.path('figs', 'wp_grid_xgb.png'),
   width = 8,
   height = 6
 )
-
-## usage ----
-augment.cod_wp_model <- function(x, data, ...) {
-  data$wp <- predict(
-    x,
-    data,
-    ...
-  )
-  data
-}
-
-base_point_df <- crossing(seconds_elapsed = 0L, opponent_diff = 0L, is_offense = c('yes', 'no'))
-pred_base_points <- bind_rows(
-  augment(pre_plant_model, base_point_df) |> mutate(is_pre_plant = TRUE),
-  augment(post_plant_model, base_point_df) |> mutate(is_pre_plant = FALSE)
-)
-
-add_wpa <- function(data, is_pre_plant = TRUE) {
-  pred_base_point <- pred_base_points |> filter(is_pre_plant == !!is_pre_plant)
-  data |> 
-    mutate(
-      across(
-        wp,
-        ~case_when(
-          !is.na(.x) ~ .x,
-          win_round == 'yes' ~ 1,
-          win_round == 'no' ~ 0
-        )
-      )
-    ) |> 
-    left_join(
-      pred_base_point |> select(is_offense, default_wp = wp),
-      by = 'is_offense'
-    ) |> 
-    group_by(round_id, is_offense) |> 
-    mutate(
-      wpa = wp - coalesce(lag(wp), default_wp)
-    ) |> 
-    ungroup() |> 
-    select(-default_wp)
-}
-
-pre_plant_pbp <- pre_plant_model |> 
-  augment(pre_plant_pbp) |> 
-  add_wpa(is_pre_plant = TRUE)
-
-post_plant_pbp <- post_plant_model |> 
-  augment(post_plant_pbp) |> 
-  add_wpa(is_pre_plant = FALSE)
-
-
-pbp_wp <- bind_rows(
-  pre_plant_pbp,
-  post_plant_pbp
-)
-
-## summarize ----
-pbp_wp |> filter(round_id == '2021-SND-011-01')
-
-full_join(
-  pbp_wp |> 
-    group_by(player = killer_player, is_post_plant) |> 
-    summarize(
-      kills = n(),
-      wpa_kills = sum(wpa)
-    ) |> 
-    ungroup(),
-  pbp_wp |> 
-    group_by(player = victim_player, is_post_plant) |> 
-    summarize(
-      deaths = n(),
-      wpa_deaths = sum(-wpa)
-    ) |> 
-    ungroup(),
-  by = c('player', 'is_post_plant')
-) |> 
-  mutate(
-    across(is_post_plant, ~ifelse(.x, 'post_plant', 'pre_plant'))
-  ) |> 
-  pivot_wider(
-    names_from = is_post_plant,
-    values_from = c(kills, wpa_kills, deaths, wpa_deaths)
-  ) |> 
-  mutate(
-    wpa_total = wpa_kills_pre_plant + wpa_kills_post_plant + wpa_deaths_pre_plant + wpa_deaths_post_plant
-  ) |> 
-  arrange(desc(wpa_total))
-
-pbp_wp |> 
-  filter(killer_player == 'Shotzzy') |> 
-  ggplot() +
-  aes(x = wpa) +
-  geom_histogram()
-
-
--- scripts/helpers-wp.R
+++ scripts/helpers-wp.R
@@ -1,10 +1,10 @@
 
-max_quietly <- function(...) {
-  f <- quietly(max)
-  f(...)$result
-}
+## generic ----
+target_name <- 'win_round'
+default_side <- 'o'
 
-generate_seconds_grid <- function(is_pre_plant = TRUE) {
+## TODO: Add validation for is_pre_plant
+generate_seconds_grid <- function(is_pre_plant) {
   if (isTRUE(is_pre_plant)) {
     # min_second <- c(seq(0, 30, 3), seq(30, 60, 2), seq(50, 75, 1), seq(75, 85, 1), seq(86, 90))
     # max_second <- c(seq(15, 45, 3), seq(40, 70, 2), seq(60, 85, 1), seq(80, 90, 1), rep(90, 5))
@@ -20,17 +20,301 @@ generate_seconds_grid <- function(is_pre_plant = TRUE) {
   )
 }
 
-get_max_second <- function(is_pre_plant = TRUE) {
+get_max_second <- function(is_pre_plant) {
   ifelse(isTRUE(is_pre_plant), 90L, 45L)
 }
 
-safely_glm <- safely(glm)
-library(recipes)
-model_window_wp <- function(pbp_grid, seconds_elapsed1, seconds_elapsed2) {
-  filt <- pbp_grid |> 
-    filter(seconds_elapsed >= seconds_elapsed1, seconds_elapsed <= seconds_elapsed2)
+## TODO: 
+## - is_initial_bomb_carrier_killed should only be a feature for pre plant
+## - is_during_attempted fields should only be relevant if player planting / defusing is killed
+get_extra_feature <- function(is_pre_plant) {
+  extra_features <- c('plant' = -1, 'defuse' = 1)
+  extra_feature_name <- ifelse(isTRUE(is_pre_plant), 'plant', 'defuse')
+  extra_feature <- extra_features[extra_feature_name]
+  names(extra_feature) <- sprintf('is_during_attempted_%s', extra_feature_name)
+  extra_feature
+}
 
-  msg <- sprintf('for `seconds_elapsed >= %s & seconds_elapsed <= %s.', seconds_elapsed1, seconds_elapsed2)
+## TODO: Add more validation for `method`
+get_all_features <- function(is_pre_plant, named, method = NULL) {
+  
+  if (!is.null(method)) {
+    stopifnot('`method` must be one of "lb" or "xgb" if not `NULL`' = method %in% c('lb', 'xgb'))
+  }
+  
+  extra_feature <- get_extra_feature(is_pre_plant = is_pre_plant)
+  
+  all_features <- c(
+    'seconds_elapsed' = 1,
+    'prev_opponent_diff' = -1,
+    'is_initial_bomb_carrier_killed' = -1,
+    extra_feature
+  )
+  
+  if (isFALSE(named)) {
+    all_features <- names(all_features)
+  }
+  
+  if (is.null(method) || method == 'xgb') {
+    return(all_features)
+  }
+  
+  ## if method == 'lb'
+  setdiff(all_features, 'seconds_elapsed')
+}
+
+get_lb_window_feature_names <- function(is_pre_plant) {
+  feature_names <- get_all_features(
+    is_pre_plant = is_pre_plant, 
+    named = FALSE,
+    method = 'lb'
+  )
+  setdiff(feature_names, 'seconds_elapsed')
+}
+
+validate_colnames <- function(data) {
+  all_feature_names <- c(TRUE, FALSE) |> 
+    purrr::map(
+      ~get_all_features(
+        is_pre_plant = .x, 
+        named = FALSE
+      )
+    ) |> 
+    purrr::flatten_chr() |> 
+    unique()
+  
+  stopifnot(
+    all(c('is_pre_plant', 'side', all_feature_names) %in% colnames(data))
+  )
+  invisible(data)
+}
+
+split_model_data <- function(data) {
+  
+  validate_colnames(data = data)
+  
+  model_data <- dplyr::filter(data, .data[['side']] == default_side)
+  
+  list(
+    'pre' = dplyr::filter(model_data, .data[['is_pre_plant']]),
+    'post' = dplyr::filter(model_data, !.data[['is_pre_plant']])
+  )
+}
+
+fit_wp_model_states <- function(data, method, ...) {
+  
+  model_data <- split_model_data(data = data)
+  
+  ## TODO: Use invoke?
+  f <- switch(
+    method,
+    'lb' = fit_wp_model_lb_state,
+    'xgb' = fit_wp_model_xgb_state
+  )
+  
+  ## things get messy if using imap here, so just be explicit instead
+  models <- list(
+    'pre' = f(model_data[['pre']], is_pre_plant = TRUE, ...),
+    'post' = f(model_data[['post']], is_pre_plant = FALSE, ...)
+  )
+
+  class(models) <- c('wp_model', class(models))
+  models
+}
+
+generate_pred_grid <- function(is_pre_plant) {
+  all_feature_names <- get_all_features(
+    is_pre_plant = is_pre_plant, 
+    named = FALSE
+  )
+  binary_feature_names <- all_feature_names[grepl('^is', all_feature_names)]
+  
+  max_second <- get_max_second(is_pre_plant)
+  max_diff <- 3L
+  
+  binary_l <- vector(mode = 'list', length(binary_feature_names))
+  names(binary_l) <- binary_feature_names
+  for(el in names(binary_l)) {
+    binary_l[[el]] <- c(0L, 1L)
+  }
+  
+  extra_feature <- get_extra_feature(is_pre_plant = !is_pre_plant)
+  
+  grid <- tidyr::crossing(
+    !!!append(
+      ## TODO:
+      ## is it possible for me to not hard-code this? 
+      ##   maybe another function similar to get_all_feature_names where a named vector of values can be returned?
+      list(
+        'side' = c('o', 'd'),
+        'is_pre_plant' = is_pre_plant,
+        'seconds_elapsed' = 0L:(max_second - 1L),
+        'prev_opponent_diff' = -max_diff:max_diff
+      ),
+      binary_l
+    )
+  )
+  grid[[names(extra_feature)]] <- 0L
+  grid
+}
+
+predict.wp_model <- function(object, new_data, ...) {
+  
+  validate_colnames(data = new_data)
+  
+  pred <- ifelse(
+    new_data[['is_pre_plant']],
+    predict(object[['pre']], new_data, ...),
+    predict(object[['post']], new_data, ...)
+  )
+  
+  ifelse(
+    new_data[['side']] == 'o',
+    pred,
+    1 - pred
+  )
+}
+
+augment.wp_model <- function(x, data, ...) {
+  data[['wp']] <- predict(x, data, ...)
+  data
+}
+
+generate_wp_state_grid <- function(model, is_pre_plant) {
+  
+  pred_grid <- generate_pred_grid(is_pre_plant = is_pre_plant)
+  
+  augment(
+    model,
+    pred_grid
+  )
+}
+
+generate_wp_grid <- function(model) {
+  wp_grid <- c(TRUE, FALSE) |> 
+    purrr::map_dfr(
+      ~generate_wp_state_grid(
+        model,
+        is_pre_plant = .x
+      )
+    )
+  class(wp_grid) <- c('wp_grid', class(wp_grid))
+  wp_grid
+}
+
+summarize_pred_grid_across_features <- function(df, binary_feature_name) {
+  df |> 
+    group_by(
+      .data[['is_pre_plant']],
+      .data[['seconds_elapsed']],
+      .data[['prev_opponent_diff']],
+      value = .data[[binary_feature_name]]
+    ) |> 
+    summarize(
+      'wp' = mean(.data[['wp']])
+    ) |> 
+    ungroup()
+}
+
+# ggsci::pal_tron('legacy', 1)(7)
+# RColorBrewer::brewer.pal(7, 'PRGn') |> datapasta::vector_paste_vertical()
+
+player_diff_pal <- c(
+  '#762A83',
+  '#AF8DC3',
+  '#E7D4E8',
+  '#F7F7F7',
+  '#D9F0D3',
+  '#7FBF7B',
+  '#1B7837'
+) |> 
+  rlang::set_names(as.character(seq(-3, 3, by = 1)))
+
+autoplot.wp_grid <- function(data) {
+  filt <- dplyr::filter(data, .data[['side']] == default_side)
+  df <- c(
+    'is_initial_bomb_carrier_killed',
+    'is_during_attempted_defuse', 
+    'is_during_attempted_plant'
+  ) |> 
+    rlang::set_names() |> 
+    purrr::map_dfr(
+      ~summarize_pred_grid_across_features(
+        filt, 
+        .x
+      ), 
+      .id = 'feature'
+    ) |> 
+    dplyr::mutate(
+      'feature_value' = 
+        sprintf(
+          '%s: %s', 
+          .data[['feature']], 
+          ifelse(.data[['value']] == 1, 'yes', 'no')
+        )
+    )
+  
+  pre <- df |> filter(is_pre_plant)
+  max_pre_plant_second <- 90L
+  post <- df |> 
+    filter(!is_pre_plant) |> 
+    mutate(
+      'seconds_elapsed' = seconds_elapsed + max_pre_plant_second
+    )
+  
+  dplyr::bind_rows(
+    pre,
+    post
+  ) |> 
+    ggplot2::ggplot() +
+    ggplot2::aes(x = .data[['seconds_elapsed']], y = .data[['wp']]) +
+    ggplot2::geom_hline(
+      color = 'white',
+      ggplot2::aes(yintercept = 0.5)
+    ) +
+    ggplot2::geom_vline(
+      color = 'white',
+      ggplot2::aes(xintercept = max_pre_plant_second)
+    ) +
+    ggplot2::geom_step(
+      size = 1,
+      ggplot2::aes(color = factor(.data[['prev_opponent_diff']]))
+    ) +
+    ggplot2::guides(
+      color = ggplot2::guide_legend(
+        title = 'Net # of players',
+        override.aes = list(size = 3)
+      )
+    ) +
+    # ggsci::scale_color_tron() +
+    ggplot2::scale_color_manual(values = player_diff_pal) +
+    ggplot2::scale_y_continuous(labels = scales::percent) +
+    ggplot2::facet_wrap(~.data[['feature_value']], dir = 'v', nrow = 2) +
+    ggplot2::coord_cartesian(ylim = c(0, 1)) +
+    ggplot2::theme(
+      legend.position = 'top'
+    ) +
+    ggplot2::labs(
+      title = 'Offensive Win Probability',
+      x = 'Seconds Elapsed',
+      y = 'Win Probability'
+    )
+}
+
+## lb approach ----
+safely_glm <- purrr::safely(glm)
+fit_window_wp_coefs <- function(data, earlist_seconds_elapsed, latest_seconds_elapsed, is_pre_plant) {
+  filt <- dplyr::filter(
+    data,
+    .data[['seconds_elapsed']] >= earlist_seconds_elapsed, 
+    .data[['seconds_elapsed']] <= latest_seconds_elapsed
+  )
+  
+  msg <- sprintf(
+    'for `seconds_elapsed >= %s & seconds_elapsed <= %s.',
+    earlist_seconds_elapsed, 
+    latest_seconds_elapsed
+  )
   if (nrow(filt) == 0) {
     message(sprintf('No data %s', msg))
     return(NULL)
@@ -40,43 +324,23 @@ model_window_wp <- function(pbp_grid, seconds_elapsed1, seconds_elapsed2) {
     warning(sprintf('Only one unique value in target variable %s', msg))
   } 
   
-  binary_terms <- c(
-    'is_offense',
-    'is_initial_bomb_carrier_killed',
-    'is_during_attempted_plant',
-    'is_during_attempted_defuse'
+  feature_names <- get_lb_window_feature_names(is_pre_plant = is_pre_plant)
+  
+  default_result <- rlang::set_names(
+    rep(NA, length(feature_names)), 
+    feature_names
   )
   
-  binary_terms_to_keep <- binary_terms |> 
-    keep(
-      ~{
-        # has_more_than_one_value <- length(unique(filt[[.x]])) > 1L
-        # if (isTRUE(has_more_than_one_value)) {
-        #   return(TRUE)
-        # } 
-        # warning(sprintf('Only one unique value in feature `%s` %s', .x, msg))
-        # return(FALSE)
-        length(unique(filt[[.x]])) > 1L
-      }
-    )
-  
-  all_terms <- c(
-    'prev_opponent_diff',
-    binary_terms_to_keep
-  )
-  
-  default_result <- set_names(rep(NA, length(all_terms)), all_terms)
-
+  form <- sprintf('%s ~ . - 1L', target_name)
   fit <- safely_glm(
-    win_round ~ ., 
-    data = filt |> select(win_round, all_of(all_terms)), 
+    as.formula(form), 
+    data = dplyr::select(
+      filt, 
+      .data[['win_round']], 
+      tidyselect::vars_select_helpers$all_of(feature_names)
+    ), 
     family = binomial(link = 'logit')
   )
-  fit <- recipe(
-    win_round ~ .,
-    data = filt |> select(win_round, all_of(all_terms))
-  ) |> 
-    step_interact(everything())
   
   if (!is.null(fit$error)) {
     message(sprintf('Error %s.\n%s', msg, fit$error))
@@ -85,23 +349,24 @@ model_window_wp <- function(pbp_grid, seconds_elapsed1, seconds_elapsed2) {
   coefficients(fit$result)
 }
 
-estimate_window_coefs <- function(pbp_grid, is_pre_plant = TRUE, overwrite = FALSE) {
-  
-  prefix <- ifelse(is_pre_plant, 'pre', 'post')
-  path <- file.path('data', sprintf('%s_plant_wp_coefs.csv', prefix))
-  if (file.exists(path) & isFALSE(overwrite)) {
-    return(read_csv(path))
-  }
+estimate_window_coefs <- function(data, is_pre_plant) {
   
   seconds_grid <- generate_seconds_grid(is_pre_plant = is_pre_plant)
   
-  coefs <- seconds_grid |>
-    mutate(
-      coefs = map2(min_second, max_second, ~model_window_wp(pbp_grid, ..1, ..2))
+  seconds_grid |>
+    dplyr::mutate(
+      'coefs' = purrr::map2(
+        .data[['min_second']], 
+        .data[['max_second']], 
+        ~fit_window_wp_coefs(
+          data = data, 
+          earlist_seconds_elapsed = ..1, 
+          latest_seconds_elapsed = ..2,
+          is_pre_plant = is_pre_plant
+        )
+      )
     ) |> 
-    unnest_wider(coefs)
-  write_csv(coefs, path)
-  coefs
+    tidyr::unnest_wider(.data[['coefs']])
 }
 
 fit_coef_model <- function(data, y) {
@@ -111,122 +376,142 @@ fit_coef_model <- function(data, y) {
   )
 }
 
-fit_wp_model <- function(data, coefs = setdiff(colnames(data), sprintf('%s_second', c('min', 'max')))) {
-  models <- coefs |> 
-    set_names() |> 
-    map(
-      ~fit_coef_model(data, y = .x)
+fit_wp_model_lb_state <- function(data, is_pre_plant) {
+  
+  coefs <- estimate_window_coefs(
+    data, 
+    is_pre_plant = is_pre_plant
+  )
+  
+  feature_names <- get_lb_window_feature_names(is_pre_plant = is_pre_plant)
+  
+  models <- feature_names |> 
+    rlang::set_names() |> 
+    purrr::map(
+      ~fit_coef_model(coefs, y = .x)
     )
   
-  class(models) <- c('cod_wp_model', class(models))
+  class(models) <- c('wp_model_lb_state', class(models))
   models
 }
 
-predict.cod_wp_model <- function(object, data, ...) {
+predict.wp_model_lb_state <- function(object, new_data, ...) {
   
   nms <- names(object)
-  n_row <- nrow(data)
+  n_row <- nrow(new_data)
   n_col <- length(nms)
   m <- matrix(rep(0L, times = n_row * n_col), nrow = n_row, ncol = n_col)
   
   colnames(m) <- nms
   for(el in nms) {
-    m[, el] <- predict(object[[el]], newdata = data$seconds_elapsed)
+    m[, el] <- predict(object[[el]], newdata = new_data$seconds_elapsed)
   }
   
   ## Check that they are in the same order
-  stopifnot(colnames(m) == colnames(data[, nms]))
+  stopifnot(colnames(m) == colnames(new_data[, nms]))
   
-  log_odds <- rowSums(m * as.matrix(data[, nms]))
+  log_odds <- rowSums(m * as.matrix(new_data[, nms]))
   plogis(log_odds) ## logit
 }
 
-generate_pred_grid <- function(model, is_pre_plant = TRUE) {
-  nms <- fit$fit$fit$fit$feature_names
-  binary_features <- setdiff(nms, c('seconds_elapsed', 'prev_opponent_diff'))
-  
-  max_second <- get_max_second(is_pre_plant)
-  max_diff <- 3L
-  
-  binary_l <- vector(mode = 'list', length(binary_features))
-  names(binary_l) <- binary_features
-  for(el in names(binary_l)) {
-    binary_l[[el]] <- c(0L, 1L)
-  }
-  
-  crossing(
-    !!!append(
-      list(
-        seconds_elapsed = 0L:(max_second - 1L),
-        prev_opponent_diff = -max_diff:max_diff
-      ),
-      binary_l
-    )
-  )
+
+fit_wp_model_lb <- function(data, ...) {
+  fit_wp_model_states(data = data, method = 'lb', ...)
 }
 
-generate_wp_grid <- function(model, is_pre_plant = TRUE) {
-  nms <- names(model)
-  binary_features <- setdiff(nms, c('prev_opponent_diff', '(Intercept)'))
+## xgb approach ----
+options(yardstick.event_level = 'second') ## maybe i should just withr::with_seed in functions?
+fit_wp_model_xgb_state <- function(data, is_pre_plant, tune = FALSE) {
   
-  max_second <- get_max_second(is_pre_plant)
-  max_diff <- 3L
-  
-  binary_l <- vector(mode = 'list', length(binary_features))
-  names(binary_l) <- binary_features
-  for(el in names(binary_l)) {
-    binary_l[[el]] <- c(0L, 1L)
-  }
-  
-  pred_grid <- crossing(
-    !!!append(
-      list(
-        seconds_elapsed = 0L:(max_second - 1L),
-        prev_opponent_diff = -max_diff:max_diff
-      ),
-      binary_l
-    )
+  all_features <- get_all_features(
+    is_pre_plant = is_pre_plant,
+    named = TRUE
   )
   
-  pred_grid$`(Intercept)` <- 1L
-  pred_grid$wp <- predict(
-    model,
-    pred_grid
-  )
-  
-  pred_grid
-}
-
-plot_wp_grid <- function(df) {
-  df |> 
-    mutate(
-      side = ifelse(is_offense == 1L, 'Offense', 'Defense')
-    ) |> 
-    ggplot() +
-    aes(x = seconds_elapsed, y = wp) +
-    geom_hline(
-      color = 'white',
-      aes(yintercept = 0.5)
-    ) +
-    geom_step(
-      size = 1.5,
-      aes(color = factor(prev_opponent_diff))
-    ) +
-    facet_wrap(~side, scales = 'fixed') +
-    guides(
-      color = guide_legend(
-        title = 'Net # of players', 
-        override.aes = list(size = 3)
+  form <- sprintf('%s ~ .', target_name)
+  rec <- recipes::recipe(
+    as.formula(form), 
+    data = data |> 
+      dplyr::select(
+        .data[['win_round']], 
+        tidyselect::vars_select_helpers$all_of(names(all_features))
       )
-    ) +
-    ggsci::scale_color_tron() +
-    scale_y_continuous(labels = scales::comma) +
-    coord_cartesian(ylim = c(0, 1)) +
-    theme(
-      legend.position = 'top'
-    ) +
-    labs(
-      x = 'Seconds Elapsed',
-      y = 'Win Probability'
+  )
+  
+  if (isTRUE(tune)) {
+    trees <- tune::tune()
+    min_n <- tune::tune()
+    # tree_depth <- tune::tune()
+    # sample_size <- tune::tune()
+  } else if (isTRUE(is_pre_plant)) {
+    trees <- 150
+    min_n <- 50
+  } else if (isFALSE(is_pre_plant)) {
+    trees <- 100
+    min_n <- 25
+    tree_depth <- 1
+  }
+  
+  spec <- parsnip::boost_tree(
+    mtry = length(all_features), 
+    trees = !!trees,
+    min_n = !!min_n
+  ) |> 
+    parsnip::set_mode('classification') |> 
+    parsnip::set_engine(
+      monotone_constraints = !!unname(all_features),
+      engine = 'xgboost'
     )
+  
+  wf <- workflows::workflow(
+    preprocessor = rec, 
+    spec = spec
+  )
+  
+  if (isTRUE(tune)) {
+    
+    withr::local_seed(42)
+    folds <- rsample::group_vfold_cv(
+      data,
+      v = 5,
+      group = .data[['round_id']]
+    )
+    
+    n_row <- nrow(data)
+    param_grid <- tidyr::crossing(
+      # tree_depth = c(1, 2, 3, 4, 8),
+      # sample_size = round(n_row / 100, n_row / 50, n_row / 10)
+      trees = c(10, 50, 100, 150, 250, 500),
+      min_n  = c(2, 5, 10, 25, 50, 100)
+    )
+    
+    ms <- yardstick::metric_set(yardstick::roc_auc)
+    
+    tune_res <- tune::tune_grid(
+      wf,
+      resamples = folds,
+      grid = param_grid,
+      control = tune::control_grid(verbose = TRUE),
+      metrics = ms
+    )
+    
+    best_met <- tune::select_best(tune_res, 'roc_auc')
+    print(best_met) ## so that we know what to update the hard-coded values to
+    wf <- tune::finalize_workflow(wf, best_met)
+  }
+  
+  model <- parsnip::fit(wf, data)
+  class(model) <- c('wp_model_xgb_state', class(model))
+  model
+}
+
+## Could use `NextMethod()`, but i want a vector returned
+predict.wp_model_xgb_state <- function(object, new_data, type = 'prob', ...) {
+  class(object) <- setdiff(class(object), 'wp_model_xgb_state')
+  ## go to `predict.workflow`
+  predict(object, new_data, type = type, ...)[, 2][[1]]
+}
+
+fit_wp_model_xgb <- function(data, ...) {
+  fit_wp_model_states(data = data, method = 'xgb', ...)
 }
