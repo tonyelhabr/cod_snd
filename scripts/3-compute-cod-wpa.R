b4be3b2757a42cfcaf46f860cf2274870c5ef814
Tony ElHabr <anthonyelhabr@gmail.com>
2022-12-01 08:15:36 -0600

    refactor round plotter

--- scripts/3-compute-cod-wpa.R
+++ scripts/3-compute-cod-wpa.R
@@ -5,39 +5,52 @@ library(dplyr)
 source('scripts/helpers-wp.R')
 source('scripts/helpers-plot.R')
 model_lb <- qs::qread(file.path('data', 'wp_model-lb.qs'))
-all_model_pbp <- qs::qread(file.path('data', 'wp_model_data.qs'))
-
-ex <- all_model_pbp |> 
-  filter(round_id == first(round_id))
-
-ex$wp <- predict(model_lb, ex)
-ex |> 
-  select(
-    pbp_side,
-    side,
-    is_pre_plant,
-    activity,
-    seconds_elapsed,
-    model_seconds_elapsed,
-    opponent_diff,
-    is_initial_bomb_carrier_killed,
-    is_kill_on_attempted_clinch,
-    wp
-  )
-
-all_model_pbp <- all_model_pbp |> 
+all_model_pbp <- qs::qread(file.path('data', 'wp_model_data.qs')) |> 
   mutate(
     'wp' = predict(model_lb, all_model_pbp),
     .before = 1
   )
-class(all_model_pbp) <- c('wp_model_df', class(all_model_pbp))
 
-engagement_id_n <- all_model_pbp |> count(engagement_id)
+# ex <- all_model_pbp |> 
+#   filter(round_id == '2021-SND-011-01')
+# 
+# ex$wp <- predict(model_lb, ex)
+# ex |> 
+#   select(
+#     pbp_side,
+#     side,
+#     is_pre_plant,
+#     activity,
+#     seconds_elapsed,
+#     model_seconds_elapsed,
+#     opponent_diff,
+#     is_initial_bomb_carrier_killed,
+#     is_kill_on_attempted_clinch,
+#     wp
+#   )
 
-all_model_pbp$wp <- predict(model_lb, all_model_pbp)
-engagement_ids_missing_wp <- all_model_pbp |> 
-  filter(is.na(wp)) |> 
-  distinct(engagement_id)
-stopifnot(nrow(engagement_ids) == 0L)
-autoplot(all_model_pbp, round_id = '2021-SND-011-02', side = 'o')
+# engagement_id_n <- all_model_pbp |> count(engagement_id)
+# engagement_ids_missing_wp <- all_model_pbp |> 
+#   filter(is.na(wp)) |> 
+#   distinct(engagement_id)
+# stopifnot(nrow(engagement_ids) == 0L)
+
+autoplot(
+  model_lb,
+  type = 'round',
+  data = all_model_pbp,
+  round_id = '2021-SND-011-02',
+  side = 'o',
+  expand = FALSE
+)
+
+autoplot(
+  model_lb,
+  type = 'round',
+  data = all_model_pbp,
+  round_id = '2021-SND-011-02',
+  side = 'o',
+  expand = TRUE
+)
+
 
--- scripts/helpers-wp.R
+++ scripts/helpers-wp.R
@@ -481,8 +481,149 @@ plot_wp_grid <- function(data, feature_name, ...) {
     ggplot2::facet_wrap(~.data[['feature']], dir = 'v', nrow = 2)
 }
 
+
+side_lab_switcher <- function(side) {
+  switch(
+    side,
+    'o' = 'Offensive',
+    'd' = 'Defensive'
+  )
+}
+
+plot_round <- function(model, data, round_id, side = 'o', expand = FALSE) {
+  stopifnot(length(round_id) == 1)
+  side <- rlang::arg_match(side, c('o', 'd'))
+  
+  filt <- data |> 
+    dplyr::filter(.data[['round_id']] == !!round_id, .data[['side']] == !!side)
+  
+  last_row <- tail(filt, 1)
+  filt <- dplyr::add_row(
+    filt,
+    last_row |> 
+      dplyr::mutate(
+        'activity' = 'End',
+        'seconds_elapsed' = .data[['seconds_elapsed']] + 1
+      )
+  )
+  
+  if (isTRUE(expand)) {
+    f_line <- ggplot2::geom_line
+    seconds_elapsed_range <- range(filt[['seconds_elapsed']])
+    pre_plant_seconds_elapsed_range <- filt |> 
+      dplyr::filter(.data[['is_pre_plant']]) |> 
+      dplyr::pull(.data[['model_seconds_elapsed']]) |> 
+      range()
+    
+    ## TODO: Does this work for non-integer seconds?
+    grid <- tibble::tibble(
+      'model_seconds_elapsed' = seq(pre_plant_seconds_elapsed_range[1], pre_plant_seconds_elapsed_range[2]),
+      'is_pre_plant' = rep(TRUE, pre_plant_seconds_elapsed_range[2] + 1)
+    ) |> 
+      dplyr::mutate('seconds_elapsed' = .data[['model_seconds_elapsed']])
+    
+    if (isTRUE(any(filt[['is_pre_plant']] == FALSE))) {
+      post_plant_seconds_elapsed_range <- filt |> 
+        dplyr::filter(!.data[['is_pre_plant']]) |> 
+        dplyr::pull(.data[['model_seconds_elapsed']]) |> 
+        range()
+      
+      grid <- bind_rows(
+        grid,
+        tibble::tibble(
+          'model_seconds_elapsed' = seq(post_plant_seconds_elapsed_range[1], post_plant_seconds_elapsed_range[2]),
+          'is_pre_plant' = rep(TRUE, post_plant_seconds_elapsed_range[2] + 1)
+        ) |> 
+          dplyr::mutate('seconds_elapsed' = pre_plant_seconds_elapsed_range[2] + .data[['model_seconds_elapsed']])
+      )
+    }
+    
+    df <- grid |> 
+      dplyr::left_join(
+        filt,
+        by = c('model_seconds_elapsed', 'seconds_elapsed', 'is_pre_plant')
+      ) |> 
+      tidyr::fill(
+        # tidyselect::vars_select_helpers$all_of(setdiff(colnames(filt), join_cols)),
+        get_all_features(is_pre_plant = TRUE, named = FALSE, method = 'lb'),
+        .direction = 'down'
+      )
+  } else {
+    f_line <- ggplot2::geom_step
+    df <- filt
+  }
+  
+  df$wp <- predict(model, df)
+  
+  if (nrow(filt) == 0) {
+    stop(
+      'Could not find round_id or side in data.'
+    )
+  }
+  
+  labels <- filt |> 
+    # dplyr::distinct(.data[['engagement_id']], .keep_all = TRUE) |> 
+    dplyr::mutate(
+      'label' = dplyr::case_when(
+        is.na(.data[['activity']]) ~ '???',
+        .data[['activity']] == 'End' ~ 'End',
+        .data[['activity']] == 'Start' ~ 'Start',
+        .data[['activity']] == 'Plant' & .data[['is_pre_plant']] ~ sprintf('%s (%s) plants (pre-plant)', .data[['activity_player']], .data[['activity_team']]),
+        .data[['activity']] == 'Plant' & !.data[['is_pre_plant']] ~ sprintf('%s (%s) plants (post-plant)', .data[['activity_player']], .data[['activity_team']]),
+        .data[['activity']] == 'Defuse' ~ sprintf('%s (%s) defuses', .data[['activity_opponent']], .data[['activity_player']]),
+        .data[['activity']] == 'Kill' ~ sprintf('%s (%s) kills\n%s (%s)', .data[['activity_player']], .data[['activity_team']], .data[['activity_opposer']], .data[['activity_opponent']]),
+        .data[['activity']] == 'Kill Planter' ~ sprintf('%s (%s) kills\n%s %s (planting)', .data[['activity_player']], .data[['activity_team']], .data[['activity_opposer']], .data[['activity_opponent']]),
+        .data[['activity']] == 'Kill Defuser' ~ sprintf('%s (%s) kills\n%s (%s) (defusing)', .data[['activity_player']], .data[['activity_team']], .data[['activity_opposer']], .data[['activity_opponent']]),
+        .data[['activity']] == 'Self Kill' ~ sprintf('%s (%s) self-kills', .data[['activity_player']], .data[['activity_team']]),
+        .data[['activity']] == 'Team Kill' ~ sprintf('%s (%s)\nteam-kills %s', .data[['activity_player']], .data[['activity_team']], .data[['activity_opposer']])
+      )
+    )
+  
+  df |> 
+    ggplot2::ggplot() +
+    ggplot2::aes(x = .data[['seconds_elapsed']], y = .data[['wp']]) +
+    # ggplot2::geom_step() +
+    f_line() +
+    ggforce::geom_mark_circle(
+      data = labels |> dplyr::filter(!(.data[['activity']] %in% c('Start', 'End'))),
+      label.colour = 'white',
+      # label.fill = 'transparent',
+      label.fill = blackish_background,
+      con.colour = 'white',
+      label.family = font,
+      label.fontsize = 12,
+      label.fontface = 'plain',
+      # label.buffer = 0,
+      # expand = 0,
+      expand = ggplot2::unit(0.01, 'npc'),
+      con.linetype = 2,
+      con.border = 'none',
+      con.type = 'straight',
+      n = 1,
+      label.buffer = ggplot2::unit(0.15, 'npc'),
+      colour = 'transparent',
+      ggplot2::aes(
+        group = .data[['label']],
+        label = .data[['label']]
+      )
+    ) +
+    ggplot2::geom_point(
+      data = labels
+    ) +
+    ggplot2::scale_y_continuous(
+      limits = c(0, 1),
+      labels = scales::percent
+    ) +
+    ggplot2::labs(
+      title = sprintf('%s win probability', side_lab_switcher(side)),
+      y = 'Win probability',
+      x = 'Seconds elapsed'
+    )
+}
+
+
 autoplot.wp_model <- function(object, type = 'grid', ...) {
-  type <- rlang::arg_match(type, c('grid', 'coefs'))
+  type <- rlang::arg_match(type, c('grid', 'coefs', 'round'))
   
   cls <- class(object)
   if (type == 'coefs' & !any(cls == 'wp_model_lb')) {
@@ -496,7 +637,8 @@ autoplot.wp_model <- function(object, type = 'grid', ...) {
   switch(
     type,
     'grid' = plot_wp_grid(generate_wp_grid(object), ...),
-    'coefs' = plot_coefs(object, ...)
+    'coefs' = plot_coefs(object, ...),
+    'round' = plot_round(object, ...)
   )
 }
 
